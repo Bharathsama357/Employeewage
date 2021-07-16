2b7c5e77055426a9c29620bfdd42f176bcb8945e
Bharathsama357 <bharathsama269@gmail.com>
2021-07-16 12:27:25 +0530

    modified : added features for wage calculation

--- empWage.sh
+++ empWage.sh
@@ -1,10 +1,16 @@
 #! /bin/bash -x
+
 #constants
 IS_PRESENT=1
 empcheck= $((RANDOM%2))
+EMP_RATE_PER_HR=20
+
 if [ $empcheck -eq $IS_PRESENT ]
 then
-echo " Employee is PRESENT "
+ empHr=8
 else
-echo " EMmployee is ABSENT"
+ empHr=0
 fi
+
+salary=$(( $empHr * $EMP_RATE_PER_HR ))
+
